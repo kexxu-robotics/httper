8caeb18d3fde7a53a2bef0cbd8fe15f0682ae2a4
Jurriaan <jurschreuder@gmail.com>
2024-02-10 20:04:01 +0100

    added test for if input is too large

--- input_json_test.go
+++ input_json_test.go
@@ -44,3 +44,54 @@ func TestBasic(t *testing.T) {
 		t.Fatalf("Expected status code 200")
 	}
 }
+
+func TestMalformed(t *testing.T) {
+	body := []byte("this is not json as you can see")
+
+	r := httptest.NewRequest(http.MethodPost, "/testing", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+
+	obj := struct {
+		A string
+		B int
+		C int64
+	}{}
+
+	err := GetJsonBodyDefault(r, w, &obj)
+	if err == nil {
+		t.Fatalf("Error read json %v", err)
+	}
+
+	res := w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != 400 {
+		t.Fatalf("Expected status code 400 got %d", res.StatusCode)
+	}
+}
+
+func TestTooBig(t *testing.T) {
+	reqObj := struct {
+		A []byte
+	}{
+		A: make([]byte, 2000),
+	}
+	body, _ := json.Marshal(reqObj)
+
+	r := httptest.NewRequest(http.MethodPost, "/testing", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+
+	obj := struct {
+		A []int
+	}{}
+
+	err := GetJsonBody(r, w, &obj, 1024, true)
+	if err == nil {
+		t.Fatalf("Should give an error, the input size is too big")
+	}
+
+	res := w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != 413 {
+		t.Fatalf("Expected status code 413 got %d", res.StatusCode)
+	}
+}
