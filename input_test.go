4e4c9aec523fdd00fc6a48ed678b4de930e868b9
Jurriaan <jurschreuder@gmail.com>
2024-02-10 19:22:44 +0100

    one more test

--- input_test.go
+++ input_test.go
@@ -23,3 +23,20 @@ func TestInt64(t *testing.T) {
 		t.Fatalf("Expected status code 200")
 	}
 }
+
+func TestInt(t *testing.T) {
+
+	r := httptest.NewRequest(http.MethodGet, "/testing?test=42", nil)
+	w := httptest.NewRecorder()
+
+	test, err := CheckFormInt(r, w, "test")
+	if err != nil || test != 42 || reflect.TypeOf(test).Kind() != reflect.Int {
+		t.Fatalf("Error read int64 %v", err)
+	}
+
+	res := w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected status code 200")
+	}
+}
